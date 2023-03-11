ad3003527fe6884f7637445ebece4bd8795ade3b
krukaleksander <kruk.aleksander@hotmail.com>
2023-03-11 07:36:47 +0100

    chore: install prettier, create prettier config file, add footer component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import './App.css';
-import {ChakraProvider} from "@chakra-ui/react";
-import {RouterProvider,} from "react-router-dom";
-import {router} from "./routes/Router";
+import React from "react";
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Router";
 import MenuMobile from "./components/menu-mobile";
 import DesktopMenu from "./components/desktop-menu";
 import AccessMenu from "./components/access-menu";
-import {useStore} from "@nanostores/react";
-import {whiteColorBackground} from "./stores/contrast.store";
+import { useStore } from "@nanostores/react";
+import { whiteColorBackground } from "./stores/contrast.store";
+import Footer from "./components/footer";
 
 function App() {
-    const bgColor = useStore(whiteColorBackground);
+  const bgColor = useStore(whiteColorBackground);
   return (
-      <ChakraProvider>
-    <div className="App" style={{backgroundColor: bgColor}}>
-        <div className='Wrapper'>
-        <MenuMobile/>
-        <AccessMenu/>
-        <DesktopMenu/>
-        <RouterProvider router={router} />
+    <ChakraProvider>
+      <div className="App" style={{ backgroundColor: bgColor }}>
+        <div className="Wrapper">
+          <MenuMobile />
+          <AccessMenu />
+          <DesktopMenu />
+          <RouterProvider router={router} />
+          <Footer />
         </div>
-    </div>
-     </ChakraProvider>
+      </div>
+    </ChakraProvider>
   );
 }
 
